8215cc18d7c39b3cbf34b3466a4a35fbdaa09bae
Fernando Arriondo <arriondovfernando@gmail.com>
2024-04-06 16:30:44 -0300

    finished setting up next-auth

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from '@/components/theme-provider'
+
+export default function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  )
+}
