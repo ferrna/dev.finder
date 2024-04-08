1c913b0834ce743e97d5164d30b5346a87a610a4
Fernando Arriondo <arriondovfernando@gmail.com>
2024-04-07 23:15:08 -0300

    header and icon

--- app/header.tsx
+++ app/header.tsx
@@ -1,19 +1,68 @@
 'use client'
 import { ModeToggle } from '@/components/mode-toggle'
-import { Button } from '@/components/ui/button'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { LogIn, LogOut } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import Image from 'next/image'
+import Link from 'next/link'
 
+function Dropdown() {
+  const session = useSession()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant={'ghost'} style={{ display: 'flex', alignItems: 'center' }}>
+          <Avatar>
+            <AvatarImage src={session.data?.user?.image ?? ''} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          &nbsp;
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {session.data ? (
+          <DropdownMenuItem onClick={() => signOut()} className="cursor-pointer">
+            <LogOut />
+            &nbsp; Sign Out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn('google')} className="cursor-pointer">
+            <LogIn />
+            &nbsp; Sign In
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 export default function Header() {
   const session = useSession()
   return (
-    <header>
-      {session.data ? (
-        <Button onClick={() => signOut()}>Sign Out</Button>
-      ) : (
-        <Button onClick={() => signIn('google')}>Sign In</Button>
-      )}
-      {session.data?.user?.name}
-      <ModeToggle />
+    <header className="py-2 bg-gray-100 dark:bg-gray-900 container mx-auto">
+      <div className="flex justify-between items-center">
+        <Link
+          href="/"
+          className="flex items-center text-xl font-medium cursor-pointer
+          text-ring dark:text-foreground hover:text-blue-400
+          dark:hover:text-blue-400"
+        >
+          <Image src="/icon.png" alt="Dev Finder" width={60} height={60} />
+          <span className="pl-1 tracking-tight">DevFinder</span>
+        </Link>
+        <div className="flex items-center gap-4">
+          <Dropdown />
+          <ModeToggle />
+        </div>
+      </div>
     </header>
   )
 }
--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,38 @@
+/*
+Before
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
 const queryClient = postgres(process.env.DATABASE_URL!)
 const db = drizzle(queryClient, { schema })
+export { db } 
+when dealing with a postgreSQL/MySQL database there's usually a limit
+of how many open connections you could have,
+but in nextjs when you save and restart the server it reopens a brand
+new connection, so you could ran into using all connections and getting
+an error that basically can be resolved by killing the server and restarting
+it but a better approach is to use a global variable to store the connection.
+The above error is dev-only and not a problem in production though.
+*/
+
+import { drizzle } from 'drizzle-orm/postgres-js'
+import * as schema from './schema'
+import postgres from 'postgres'
+import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
+
+declare global {
+  var db: PostgresJsDatabase<typeof schema> | undefined
+}
+let db: PostgresJsDatabase<typeof schema>
+
+if (process.env.NODE_ENV === 'production') {
+  db = drizzle(postgres(process.env.DATABASE_URL!), { schema })
+} else {
+  if (!global.db) {
+    global.db = drizzle(postgres(process.env.DATABASE_URL!), { schema })
+  }
+  db = global.db
+}
+
 export { db }
