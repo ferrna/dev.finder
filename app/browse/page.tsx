7095892bba145be1a9c4bb9a419f2d46c633bfd8
Fernando Arriondo <arriondovfernando@gmail.com>
2024-06-21 16:02:53 -0300

    browse roomcard showmore feature

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,51 +1,10 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { Room } from '@/db/schema'
-import { GithubIcon } from 'lucide-react'
 import { getRooms } from '@/data-access/rooms'
-import { ListTags } from '@/components/listTags'
 import SearchBar from './search-bar'
 import { unstable_noStore } from 'next/cache'
-import { splitTags } from '@/lib/utils'
 import Image from 'next/image'
-
-function RoomCard({ room }: { room: Room }) {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{room.name}</CardTitle>
-        <CardDescription>{room.description}</CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <ListTags tags={splitTags(room?.tags)} />
-        {room.githubRepo && (
-          <Link
-            href={`/room/${room.id}`}
-            target="_blank"
-            className="flex items-center"
-            rel="noopener noreferrer"
-          >
-            <GithubIcon className="w-6 h-6" />
-            Github Project
-          </Link>
-        )}
-      </CardContent>
-      <CardFooter>
-        <Button asChild>
-          <Link href={`/rooms/${room.id}`}>Join Room</Link>
-        </Button>
-      </CardFooter>
-    </Card>
-  )
-}
+import RoomCard from './roomCard'
 
 export default async function Browse({
   searchParams,
---
+++ app/browse/roomCard.tsx
@@ -0,0 +1,72 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Room } from '@/db/schema'
+import { GithubIcon } from 'lucide-react'
+import { ListTags } from '@/components/listTags'
+import { splitTags } from '@/lib/utils'
+import styles from './roomCard.module.css'
+import React from 'react'
+
+export default function RoomCard({ room }: { room: Room }) {
+  const ref = React.useRef<HTMLParagraphElement>(null)
+  const [showMore, setShowMore] = React.useState(false)
+  const [showLink, setShowLink] = React.useState(false)
+
+  React.useLayoutEffect(() => {
+    if (!ref.current) return
+    if (ref.current.clientHeight < ref.current.scrollHeight) {
+      setShowLink(true)
+    }
+  }, [ref])
+
+  const onClickMore = () => {
+    setShowMore(!showMore)
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{room.name}</CardTitle>
+        <CardDescription
+          className={styles.description + (showMore ? styles.showall : '')}
+          ref={ref}
+        >
+          {room.description}
+        </CardDescription>
+        {showLink && (
+          <span className={styles.more} onClick={onClickMore}>
+            {showMore ? 'Show less' : 'Show more'}
+          </span>
+        )}
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <ListTags tags={splitTags(room?.tags)} />
+        {room.githubRepo && (
+          <Link
+            href={`/room/${room.id}`}
+            target="_blank"
+            className="flex items-center"
+            rel="noopener noreferrer"
+          >
+            <GithubIcon className="w-6 h-6" />
+            Github Project
+          </Link>
+        )}
+      </CardContent>
+      <CardFooter>
+        <Button asChild>
+          <Link href={`/rooms/${room.id}`}>Join Room</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
